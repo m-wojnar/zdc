32fb8d1c0c1106d8345c7d89407540fc8d2a8162
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-03-05 10:58:36 +0100

    Add decoder only model and Supervised AE with channel loss

---
+++ zdc/models/autoencoder/supervised_channels.py
@@ -0,0 +1,94 @@
+import os
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+import optax
+from tqdm import trange
+
+from zdc.models.autoencoder.supervised import SupervisedAE, SupervisedAEGen
+from zdc.utils.data import load, batches
+from zdc.utils.losses import mse_loss, mae_loss, wasserstein_loss
+from zdc.utils.metrics import Metrics
+from zdc.utils.nn import init, forward, gradient_step, save_model, print_model
+from zdc.utils.wasserstein import sum_channels_parallel
+
+
+def loss_fn(params, state, key, img, cond, model, cond_weight):
+    (reconstructed, encoder_cond), state = forward(model, params, state, key, img)
+    mse_cond = mse_loss(cond, encoder_cond)
+    mse_rec = mse_loss(img, reconstructed)
+    ch_true, ch_pred = sum_channels_parallel(img), sum_channels_parallel(reconstructed)
+    mse_ch = mse_loss(ch_true, ch_pred)
+    return cond_weight * mse_cond + mse_rec + mse_ch, (state, mse_cond, mse_rec, mse_ch)
+
+
+def eval_fn(params, state, key, img, cond, model, cond_weight, n_reps):
+    def _eval_fn(subkey):
+        (reconstructed, encoder_cond), _ = forward(model, params, state, subkey, img, False)
+        ch_true, ch_pred = sum_channels_parallel(img), sum_channels_parallel(reconstructed)
+        mse_cond = mse_loss(cond, encoder_cond)
+        mse_rec = mse_loss(img, reconstructed)
+        mse_ch = mse_loss(ch_true, ch_pred)
+        mae = mae_loss(ch_true, ch_pred) / 5
+        wasserstein = wasserstein_loss(ch_true, ch_pred)
+        return cond_weight * mse_cond + mse_rec + mse_ch, mse_cond, mse_rec, mse_ch, mae, wasserstein
+
+    results = jax.vmap(_eval_fn)(jax.random.split(key, n_reps))
+    return jnp.array(results).mean(axis=1)
+
+
+if __name__ == '__main__':
+    batch_size = 128
+    cond_weight = 1.0
+    n_reps = 5
+    lr = 1e-4
+    epochs = 100
+    seed = 42
+
+    key = jax.random.PRNGKey(seed)
+    init_key, train_key, val_key, test_key, shuffle_key, plot_key = jax.random.split(key, 6)
+
+    r_train, r_val, r_test, p_train, p_val, p_test = load('../../../data', 'standard')
+    r_sample, p_sample = jax.tree_map(lambda x: x[20:30], (r_train, p_train))
+
+    model, model_gen = SupervisedAE(), SupervisedAEGen()
+    params, state = init(model, init_key, r_sample)
+    print_model(params)
+
+    optimizer = optax.rmsprop(lr)
+    opt_state = optimizer.init(params)
+
+    train_fn = jax.jit(partial(gradient_step, optimizer=optimizer, loss_fn=partial(loss_fn, model=model, cond_weight=cond_weight)))
+    eval_fn = jax.jit(partial(eval_fn, model=model, cond_weight=cond_weight, n_reps=n_reps))
+    eval_metrics = ('loss', 'mse_cond', 'mse_rec', 'mse_ch', 'mae', 'wasserstein')
+
+    metrics = Metrics(job_type='train', name='supervised')
+    os.makedirs('checkpoints/supervised', exist_ok=True)
+
+    for epoch in trange(epochs, desc='Epochs'):
+        shuffle_key, shuffle_train_subkey, shuffle_val_subkey = jax.random.split(shuffle_key, 3)
+
+        for batch in batches(r_train, p_train, batch_size=batch_size, shuffle_key=shuffle_train_subkey):
+            train_key, subkey = jax.random.split(train_key)
+            params, opt_state, loss, (state, mse_cond, mse_rec, mse_ch) = train_fn(params, (state, subkey, *batch), opt_state)
+            metrics.add({'loss': loss, 'mse_cond': mse_cond, 'mse_rec': mse_rec, 'mae_ch': mse_ch}, 'train')
+
+        metrics.log(epoch)
+
+        for batch in batches(r_val, p_val, batch_size=batch_size, shuffle_key=shuffle_val_subkey):
+            val_key, subkey = jax.random.split(val_key)
+            metrics.add(dict(zip(eval_metrics, eval_fn(params, state, subkey, *batch))), 'val')
+
+        metrics.log(epoch)
+
+        plot_key, subkey = jax.random.split(plot_key)
+        metrics.plot_responses(r_sample, forward(model_gen, params, state, subkey, p_sample)[0], epoch)
+
+        save_model(params, state, f'checkpoints/supervised/epoch_{epoch + 1}.pkl.lz4')
+
+    for batch in batches(r_test, p_test, batch_size=batch_size):
+        test_key, subkey = jax.random.split(test_key)
+        metrics.add(dict(zip(eval_metrics, eval_fn(params, state, subkey, *batch))), 'test')
+
+    metrics.log(epochs)
