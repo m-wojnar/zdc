Add VAE with one-hot encoding, fix gradients in VQ-VAE

zdc/models/autoencoder/vq_vae.py
@@ -56,18 +56,18 @@ class VQVAE(nn.Module):
         discrete = jax.nn.one_hot(discrete, self.num_embeddings)
         quantized = jnp.dot(discrete, self.codebook.embedding)
         quantized = quantized.reshape(encoded.shape)
-        reconstructed = self.decoder(quantized, training=training)
+        quantized_sg = encoded + jax.lax.stop_gradient(quantized - encoded)
+        reconstructed = self.decoder(quantized_sg, training=training)
         return reconstructed, encoded, discrete, quantized
 
 
 def loss_fn(params, state, key, img, cond, model, commitment_cost):
     (reconstructed, encoded, discrete, quantized), state = forward(model, params, state, key, img)
-    avg_prob = jnp.mean(discrete, axis=0)
-    perplexity = jnp.exp(-jnp.sum(avg_prob * jnp.log(avg_prob + 1e-10)))
-    quantized = encoded + jax.lax.stop_gradient(quantized - encoded)
     e_loss = mse_loss(jax.lax.stop_gradient(quantized), encoded)
     q_loss = mse_loss(quantized, jax.lax.stop_gradient(encoded))
     mse = mse_loss(img, reconstructed)
+    avg_prob = jnp.mean(discrete, axis=0)
+    perplexity = jnp.exp(-jnp.sum(avg_prob * jnp.log(avg_prob + 1e-10)))
     loss = mse + commitment_cost * e_loss + q_loss
     return loss, (state, loss, mse, e_loss, q_loss, perplexity)
 

zdc/utils/data.py
@@ -6,12 +6,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
-def load(path, scaler, val_size=0.1, test_size=0.2):
+def load(path, scaler, val_size=0.1, test_size=0.2, load_pdgid=False):
     responses = jnp.load(os.path.join(path, 'data_nonrandom_responses.npz'))['arr_0'].astype(float)
     responses = responses[..., None]
     responses = jnp.log(responses + 1)
 
     particles = jnp.load(os.path.join(path, 'data_nonrandom_particles.npz'))['arr_0'].astype(float)
+    particles, pdgid = particles[..., :-1], particles[..., -1]
 
     if scaler == 'standard':
         particles = StandardScaler().fit_transform(particles)
@@ -19,6 +20,9 @@ def load(path, scaler, val_size=0.1, test_size=0.2):
         particles = MinMaxScaler().fit_transform(particles)
     elif scaler != 'none':
         raise ValueError('Unknown scaler')
+
+    if load_pdgid:
+        particles = jnp.concatenate([particles, pdgid[..., None]], axis=-1)
 
     r_train, r_test, p_train, p_test = train_test_split(responses, particles, test_size=test_size, shuffle=False)
     r_train, r_val, p_train, p_val = train_test_split(r_train, p_train, test_size=val_size / (1 - test_size), shuffle=False)