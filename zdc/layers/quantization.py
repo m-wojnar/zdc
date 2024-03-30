4177c2b8dcc8e54122b30425db5b0621c1e2e492
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-03-30 18:35:57 +0100

    Fix EMA quantization

--- zdc/layers/quantization.py
+++ zdc/layers/quantization.py
@@ -64,30 +64,29 @@ class VectorQuantizerEMA(nn.Module):
 
     def setup(self):
         self.codebook = self.variable(
-            'state', 'embedding',
-            jax.nn.initializers.variance_scaling(1.0, 'fan_in', 'normal', out_axis=0),
+            'state', 'codebook',
+            jax.nn.initializers.normal(1.0),
             self.make_rng('zdc'), (self.num_embeddings, self.embedding_dim),
         )
         self.ema_count = self.variable(
             'stats',  'ema_count',
             jax.nn.initializers.zeros,
-            self.make_rng('zdc'), self.num_embeddings
+            jax.random.PRNGKey(0), self.num_embeddings
         )
         self.ema_weight = self.variable(
             'stats', 'ema_weight',
-            jax.nn.initializers.variance_scaling(1 / 3, 'fan_in', 'uniform'),
-            self.make_rng('zdc'), (self.num_embeddings, self.embedding_dim)
+            lambda: self.codebook.value
         )
 
-    def update_embeddings(self, ema_count, ema_weight, discrete, x_flatten, batch_size):
+    def update_embeddings(self, ema_count, ema_weight, discrete, x_flatten):
         ema_count = ema_count * self.decay + jnp.sum(discrete, axis=0) * (1 - self.decay)
-        ema_count = (ema_count + self.epsilon) / (batch_size + self.num_embeddings * self.epsilon) * batch_size
+        n = jnp.sum(ema_count)
+        ema_count = (ema_count + self.epsilon) / (n + self.num_embeddings * self.epsilon) * n
         ema_weight = ema_weight * self.decay + jnp.dot(discrete.T, x_flatten) * (1 - self.decay)
-        embeddings = ema_weight / ema_count[:, None]
-        return ema_count, ema_weight, embeddings
+        codebook = ema_weight / ema_count[:, None]
+        return ema_count, ema_weight, codebook
 
     def __call__(self, x, training=True):
-        batch_size = x.shape[0]
         x_flatten = x.reshape(-1, self.embedding_dim)
 
         distances = (
@@ -103,7 +102,7 @@ class VectorQuantizerEMA(nn.Module):
 
         if training:
             self.ema_count.value, self.ema_weight.value, self.codebook.value = jax.lax.stop_gradient(
-                self.update_embeddings(self.ema_count.value, self.ema_weight.value, discrete, x_flatten, batch_size)
+                self.update_embeddings(self.ema_count.value, self.ema_weight.value, discrete, x_flatten)
             )
 
         return discrete, quantized
