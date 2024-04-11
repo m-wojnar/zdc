ab075b9d2b215e4d0b595ab729906a152a5cad07
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-04-11 22:21:03 +0200

    Update data loader

--- zdc/utils/data.py
+++ zdc/utils/data.py
@@ -6,7 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
-def load(path, scaler, val_size=0.1, test_size=0.2, load_pdgid=False):
+DEFAULT_PATH = '/net/pr2/projects/plgrid/plggdaisnet/mwojnar/zdc/data'
+
+
+def load(path=DEFAULT_PATH, scaler='standard', val_size=0.1, test_size=0.2, load_pdgid=False):
     responses = jnp.load(os.path.join(path, 'data_nonrandom_responses.npz'))['arr_0'].astype(float)
     responses = responses[..., None]
     responses = jnp.log(responses + 1)
@@ -14,24 +17,43 @@ def load(path, scaler, val_size=0.1, test_size=0.2, load_pdgid=False):
     particles = jnp.load(os.path.join(path, 'data_nonrandom_particles.npz'))['arr_0'].astype(float)
     particles, pdgid = particles[..., :-1], particles[..., -1]
 
+    r_train, r_test, p_train, p_test, pdgid_train, pdgid_test = train_test_split(responses, particles, pdgid, test_size=test_size, shuffle=False)
+    r_train, r_val, p_train, p_val, pdgid_train, pdgid_val = train_test_split(r_train, p_train, pdgid_train, test_size=val_size / (1 - test_size), shuffle=False)
+
     if scaler == 'standard':
-        particles = StandardScaler().fit_transform(particles)
+        scaler = StandardScaler()
+        scaler.fit(p_train)
+        p_train, p_val, p_test = map(scaler.transform, (p_train, p_val, p_test))
     elif scaler == 'minmax':
-        particles = MinMaxScaler().fit_transform(particles)
+        scaler = MinMaxScaler()
+        scaler.fit(p_train)
+        p_train, p_val, p_test = map(scaler.transform, (p_train, p_val, p_test))
     elif scaler != 'none':
         raise ValueError('Unknown scaler')
 
     if load_pdgid:
-        particles = jnp.concatenate([particles, pdgid[..., None]], axis=-1)
-
-    r_train, r_test, p_train, p_test = train_test_split(responses, particles, test_size=test_size, shuffle=False)
-    r_train, r_val, p_train, p_val = train_test_split(r_train, p_train, test_size=val_size / (1 - test_size), shuffle=False)
+        p_train = jnp.concatenate([p_train, pdgid_train[..., None]], axis=-1)
+        p_val = jnp.concatenate([p_val, pdgid_val[..., None]], axis=-1)
+        p_test = jnp.concatenate([p_test, pdgid_test[..., None]], axis=-1)
 
     return r_train, r_val, r_test, p_train, p_val, p_test
 
 
-def get_samples(*xs):
-    return jax.tree_map(lambda x: x[50:100:4], xs)
+def get_samples(path=DEFAULT_PATH, scaler='standard', load_pdgid=False):
+    responses = jnp.load(os.path.join(path, 'sample_responses.npz'))['arr_0'].astype(float)
+    responses = responses[..., None]
+    responses = jnp.log(responses + 1)
+
+    particles = jnp.load(os.path.join(path, 'sample_particles.npz'))['arr_0'].astype(float)
+    particles, pdgid = particles[..., :-1], particles[..., -1]
+
+    if load_pdgid:
+        particles = jnp.concatenate([particles, pdgid[..., None]], axis=-1)
+
+    if scaler != 'standard':
+        raise ValueError('Samples were scaled with StandardScaler')
+
+    return responses, particles
 
 
 def batches(*x, batch_size, shuffle_key=None):
