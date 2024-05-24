8ad90453789b7ba75611b50960c913a7eae8467d
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-05-24 14:46:18 +0200

    Change GAN with l1 loss to l2 loss

--- zdc/models/gan/gan_l2.py
+++ zdc/models/gan/gan_l2.py
@@ -5,17 +5,17 @@ import jax.numpy as jnp
 
 from zdc.models.gan.gan import GAN, disc_loss_fn, step_fn, disc_optimizer, gen_optimizer
 from zdc.utils.data import load
-from zdc.utils.losses import xentropy_loss, mae_loss
+from zdc.utils.losses import xentropy_loss, mse_loss
 from zdc.utils.nn import init, forward, get_layers
 from zdc.utils.train import train_loop, default_generate_fn
 
 
-def gen_loss_fn(gen_params, disc_params, state, forward_key, img, cond, rand_cond, model, l1_weight=0.001):
+def gen_loss_fn(gen_params, disc_params, state, forward_key, img, cond, rand_cond, model, l2_weight=0.001):
     (generated, _, fake_output), state = forward(model, gen_params | disc_params, state, forward_key, img, cond, rand_cond)
 
     adv_loss = xentropy_loss(fake_output, jnp.ones_like(fake_output))
-    l1_loss = mae_loss(generated, img)
-    loss = adv_loss + l1_weight * l1_loss
+    l2_loss = mse_loss(generated, img)
+    loss = adv_loss + l2_weight * l2_loss
 
     gen_acc = (fake_output > 0).mean()
     return loss, (state, loss, gen_acc)
@@ -43,6 +43,6 @@ if __name__ == '__main__':
     train_metrics = ('disc_loss', 'gen_loss', 'disc_real_acc', 'disc_fake_acc', 'gen_acc')
 
     train_loop(
-        'gan_l1', train_fn, None, generate_fn, (r_train, p_train), (r_val, p_val), (r_test, p_test),
+        'gan_l2', train_fn, None, generate_fn, (r_train, p_train), (r_val, p_val), (r_test, p_test),
         train_metrics, None, params, state, (disc_opt_state, gen_opt_state), train_key
     )
