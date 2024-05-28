9db865d47f39efeb593aaae490c9406c32ebe794
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-05-28 09:43:50 +0200

    Add discriminator warmup in VQ-GAN

--- zdc/models/gan/vq_gan.py
+++ zdc/models/gan/vq_gan.py
@@ -79,19 +79,21 @@ class VQGAN(nn.Module):
         return self.generator.gen(discrete)
 
 
-def disc_loss_fn(disc_params, gen_params, state, forward_key, *x, model):
+def disc_loss_fn(disc_params, gen_params, state, forward_key, step, *x, model):
     (*_, real_output, fake_output), state = forward(model, disc_params | gen_params, state, forward_key, *x)
-
-    real_loss = xentropy_loss(real_output, jnp.ones_like(real_output))
-    fake_loss = xentropy_loss(fake_output, jnp.zeros_like(fake_output))
-    loss = real_loss + fake_loss
 
     disc_real_acc = (real_output > 0).mean()
     disc_fake_acc = (fake_output < 0).mean()
+    adv_weight = jax.lax.cond(step < 10000, lambda _: 0., lambda _: 1., None)
+
+    real_loss = xentropy_loss(real_output, jnp.ones_like(real_output))
+    fake_loss = xentropy_loss(fake_output, jnp.zeros_like(fake_output))
+    loss = adv_weight * (real_loss + fake_loss)
+
     return loss, (state, loss, disc_real_acc, disc_fake_acc)
 
 
-def gen_loss_fn(gen_params, disc_params, state, forward_key, *x, model, perceptual_loss_fn, loss_weights, commitment_cost=0.25):
+def gen_loss_fn(gen_params, disc_params, state, forward_key, step, *x, model, perceptual_loss_fn, loss_weights, commitment_cost=0.25):
     (generated, encoded, discrete, quantized, _, fake_output), state = forward(model, gen_params | disc_params, state, forward_key, *x)
     img, *_ = x
 
@@ -105,16 +107,19 @@ def gen_loss_fn(gen_params, disc_params, state, forward_key, *x, model, perceptu
     adv_loss = xentropy_loss(fake_output, jnp.ones_like(fake_output))
 
     gen_acc = (fake_output > 0).mean()
+    avg_prob = jnp.mean(discrete, axis=0)
+    perplexity = jnp.exp(-jnp.sum(avg_prob * jnp.log(avg_prob + 1e-10)))
 
     l1_weight, l2_weight, perc_weight, adv_weight = loss_weights
+    adv_weight = jax.lax.cond(step < 10000, lambda _: 0., lambda _: adv_weight, None)
     loss = vq_loss + l1_weight * l1_loss + l2_weight * l2_loss + perc_weight * perc_loss + adv_weight * adv_loss
 
-    return loss, (state, loss, vq_loss, l1_loss, l2_loss, perc_loss, adv_loss, gen_acc)
+    return loss, (state, loss, vq_loss, l1_loss, l2_loss, perc_loss, adv_loss, gen_acc, perplexity)
 
 
 def step_fn(params, carry, opt_state, disc_optimizer, gen_optimizer, disc_loss_fn, gen_loss_fn):
     state, key, img, cond = carry
-    disc_opt_state, gen_opt_state = opt_state
+    disc_opt_state, gen_opt_state, step = opt_state
     forward_key, data_key = jax.random.split(key)
 
     disc_params, gen_params = get_layers(params, 'discriminator'), get_layers(params, 'generator')
@@ -122,11 +127,11 @@ def step_fn(params, carry, opt_state, disc_optimizer, gen_optimizer, disc_loss_f
     rand_img, rand_cond = img[permutation], cond[permutation]
 
     disc_params_new, disc_opt_state, (_, *disc_losses) = gradient_step(
-        disc_params, (gen_params, state, forward_key, img, cond, rand_img, rand_cond), disc_opt_state, disc_optimizer, disc_loss_fn)
+        disc_params, (gen_params, state, forward_key, step, img, cond, rand_img, rand_cond), disc_opt_state, disc_optimizer, disc_loss_fn)
     gen_params_new, gen_opt_state, (state, *gen_losses) = gradient_step(
-        gen_params, (disc_params, state, forward_key, img, cond, rand_img, rand_cond), gen_opt_state, gen_optimizer, gen_loss_fn)
+        gen_params, (disc_params, state, forward_key, step, img, cond, rand_img, rand_cond), gen_opt_state, gen_optimizer, gen_loss_fn)
 
-    return disc_params_new | gen_params_new, (disc_opt_state, gen_opt_state), (state, *disc_losses, *gen_losses)
+    return disc_params_new | gen_params_new, (disc_opt_state, gen_opt_state, step + 1), (state, *disc_losses, *gen_losses)
 
 
 if __name__ == '__main__':
@@ -145,12 +150,12 @@ if __name__ == '__main__':
         disc_optimizer=disc_optimizer,
         gen_optimizer=gen_optimizer,
         disc_loss_fn=partial(disc_loss_fn, model=model),
-        gen_loss_fn=partial(gen_loss_fn, model=model, perceptual_loss_fn=perceptual_loss(), loss_weights=(0., 0., 0., 1.))
+        gen_loss_fn=partial(gen_loss_fn, model=model, perceptual_loss_fn=perceptual_loss(), loss_weights=(0.1 / (44 ** 2), 1.0 / (44 ** 2), 0.1, 0.1 / (6 ** 2)))
     ))
     generate_fn = jax.jit(lambda params, state, key, *x: forward(model, params, state, key, x[0], method='reconstruct')[0])
-    train_metrics = ('disc_loss', 'disc_real_acc', 'disc_fake_acc', 'gen_loss', 'vq_loss', 'l1_loss', 'l2_loss', 'perc_loss', 'adv_loss', 'gen_acc')
+    train_metrics = ('disc_loss', 'disc_real_acc', 'disc_fake_acc', 'gen_loss', 'vq_loss', 'l1_loss', 'l2_loss', 'perc_loss', 'adv_loss', 'gen_acc', 'perplexity')
 
     train_loop(
         'vq_gan', train_fn, None, generate_fn, (r_train, p_train), (r_val, p_val), (r_test, p_test),
-        train_metrics, None, params, state, (disc_opt_state, gen_opt_state), train_key
+        train_metrics, None, params, state, (disc_opt_state, gen_opt_state, 0), train_key
     )
